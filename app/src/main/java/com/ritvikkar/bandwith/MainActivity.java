9e5e6f6ebb1d37e3117e96e814da0f7348d7de1c
noah-someck <jamessomeck@yahoo.com>
2018-01-27 19:30:25 -0600

    Login Firebase code in main

--- app/src/main/java/com/ritvikkar/bandwith/MainActivity.java
+++ app/src/main/java/com/ritvikkar/bandwith/MainActivity.java
@@ -1,13 +1,129 @@
 package com.ritvikkar.bandwith;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.etEmail)
+    EditText etEmail;
+    @BindView(R.id.etPassword)
+    EditText etPassword;
+
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    @OnClick(R.id.btnRegister)
+    void registerClick() {
+        if (!isFormValid()) {
+            return;
+        }
+
+        showProgressDialog();
+
+        firebaseAuth.createUserWithEmailAndPassword(etEmail.getText().toString(), etPassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressDialog.dismiss();
+
+                if (task.isSuccessful()) {
+                    FirebaseUser fbUser = task.getResult().getUser();
+                    fbUser.updateProfile(new UserProfileChangeRequest.Builder().
+                            setDisplayName(usernameFromEmail(fbUser.getEmail())).build());
+                    Toast.makeText(MainActivity.this, "Registration ok", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void showProgressDialog() {
+        if (progressDialog == null) {
+            progressDialog = new ProgressDialog(this);
+            progressDialog.setMessage("Wait for it...");
+        }
+
+        progressDialog.show();
+
+
+    }
+
+    @OnClick(R.id.btnLogin)
+    void loginClock() {
+        if (!isFormValid()) {
+            return;
+        }
+        showProgressDialog();
+
+        firebaseAuth.signInWithEmailAndPassword(etEmail.getText().toString(), etPassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressDialog.dismiss();
+                if (task.isSuccessful()) {
+                    // open messages activity
+                    //startActivity(new Intent(MainActivity.this, PostActivity.class));
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private String usernameFromEmail(String email) {
+        if (email.contains("@")) {
+            return email.split("@")[0];
+        } else {
+            return email;
+        }
+    }
+
+    private boolean isFormValid() {
+        if (TextUtils.isEmpty(etEmail.getText())) {
+            etEmail.setError("The email cannot be empty");
+            return false;
+        }
+
+        if (TextUtils.isEmpty(etPassword.getText())) {
+            etPassword.setError("The password cannot be empty");
+            return false;
+        }
+
+        return true;
     }
 }
