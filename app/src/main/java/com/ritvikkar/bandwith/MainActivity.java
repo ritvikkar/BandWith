c71744fa598653233c76480cfd134d00ceeba9a7
Kevin Christianson <christiansonk@carleton.edu>
2018-01-28 04:36:58 -0600

    Auto stash before merge of "master" and "origin/master"

--- app/src/main/java/com/ritvikkar/bandwith/MainActivity.java
+++ app/src/main/java/com/ritvikkar/bandwith/MainActivity.java
@@ -1,9 +1,12 @@
 package com.ritvikkar.bandwith;
 
 import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -12,8 +15,11 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.Calendar;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import io.realm.Realm;
 
 public class MainActivity extends AppCompatActivity {
@@ -30,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.landing_page);
 
         Realm realm = ((MainApplication) getApplication()).getRealmItem();
         UserAccount account = realm.where(UserAccount.class).findFirst();
@@ -58,6 +64,22 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @OnClick(R.id.btnRegister)
+    public void startRegistration() {
+        setContentView(R.layout.onboarding_birthdate);
+    }
+
+    public void showDatePicker(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    public void setTime(int year, int month, int day) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month - 1, day, 0, 0);
+        ((TextView) findViewById(R.id.tvDate)).setText(calendar.getTime().toString());
     }
 
     @Override
